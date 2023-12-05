6a438ecb085f40b68ebafa346835f8a0f8ed2990
cmdbris <ebrischri@gmail.com>
2023-12-04 22:40:11 -0500

    Added javascript loading screen to Title

---
+++ Title/Title.js
@@ -0,0 +1,26 @@
+let loadingScreenStatus = false;
+let loadingScreen = document.querySelector('.Loading-Screen');
+
+
+const Navigation = document.getElementById("Navigation");
+
+loadingScreen.classList.add('Deactivate-Loading-Screen');
+
+
+// Check if the clicked element is a button with the class "Navigation-Button"
+// Extract the data-page attribute to get the page name
+// Redirect to the corresponding HTML page
+
+Navigation.addEventListener('click', (event) => {
+    const targetButton = event.target;
+    
+    if (targetButton.classList.contains('Navigation-Button')) {
+
+        loadingScreen.classList.remove('Activate-Loading-Screen', 'Deactivate-Loading-Screen');
+        void loadingScreen.offsetWidth; // Trigger reflow to ensure the animation restarts smoothly
+        loadingScreen.classList.add('Activate-Loading-Screen')
+
+        const filePath = targetButton.getAttribute('data-page');
+        setTimeout(() => {window.location.href = filePath;}, 2500);
+        }
+});
